247b63f8dde14cf1da2ac33026eaf856d135c51b
L. Bowers <31969905+Bowers-L@users.noreply.github.com>
2023-02-28 17:52:44 -0500

    Remove Cherno GLFW

--- Sigma/src/SigmaEngine/Core/EntryPoint.h
+++ Sigma/src/SigmaEngine/Core/EntryPoint.h
@@ -7,8 +7,8 @@ extern SigmaEngine::Application* SigmaEngine::CreateApplication();
 int main(int argc, char** argv) {
 	SigmaEngine::Log::Init();
 	SIGMA_CORE_INFO("Initialized Log!");
-	SIGMA_WARN("Hello!");
-	SIGMA_ERROR("This is a test of logging an error! Everything broken?? OH NO HOI");
+	//SIGMA_WARN("Hello!");
+	//SIGMA_ERROR("This is a test of logging an error! Everything broken?? OH NO HOI");
 	SigmaEngine::Application* app = SigmaEngine::CreateApplication();
 	app->Run();
 	delete app;

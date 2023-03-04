4d78a1433b47b29770b33862d2b681bac9064042
L. Bowers <31969905+Bowers-L@users.noreply.github.com>
2023-03-04 18:29:04 -0500

    Made rectangle green

--- Sigma/src/SigmaEngine/Core/Application.cpp
+++ Sigma/src/SigmaEngine/Core/Application.cpp
@@ -59,7 +59,7 @@ namespace SigmaEngine {
 
 		const Shader* shader = m_Renderer->shader();
 		shader->setUniformMat4f("u_MVP", glm::mat4(1));
-		shader->setUniform4f("u_Color", 1.f, 1.f, 1.f, 1.f);
+		shader->setUniform4f("u_Color", 0.f, 1.f, 0.f, 1.f);
 
 		m_Renderer->drawRect(m_Window->getWidth() / 2-50, m_Window->getHeight() / 2-50, 100, 100);
 		OnDraw();
